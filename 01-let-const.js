a9c11031971b87644c5e1d351c9f5dd03af3d6c2
Rayhan <prog.rayhan1@gmail.com>
2022-04-25 11:14:36 +0600

    let-const-arrow-function added

---
+++ 01-let-const.js
@@ -0,0 +1,5 @@
+let a = 5
+console.log(a);
+
+const b = 10
+console.log(b);
---
+++ 02-arrow-function.js
@@ -0,0 +1,45 @@
+// Syntax 1
+function myName(name){
+    console.log(name);
+}
+myName("rayhan")
+
+
+// Syntax 2
+const name2 = (name) =>{
+    console.log(name);
+}
+name2('reza')
+
+
+// Syntax 3
+const name3 = name =>{
+    console.log(name)
+}
+name3('yunus')
+
+
+// Syntax 4
+const name4 = () =>{
+    console.log('Nasir')
+}
+name4()
+
+
+// Syntax 5
+const name5 = (name,age) =>{
+    console.log(name,age)
+}
+name5('puson',23)
+
+
+// Syntax 6
+const multiply = (number) =>{
+    return number*2
+}
+console.log(multiply(2))
+
+
+// Syntax 7
+const multiplyy = (number) => number*2;
+console.log(multiplyy(3))
